00d920b6a7654366cc9db4145bcc9936b19c5123
Abhas Mittal <mittalabhas1@gmail.com>
2015-11-25 20:48:38 +0530

    rearrange installations
    add comments

--- index.sh
+++ index.sh
@@ -1,25 +1,24 @@
-echo "Taking super user access"
+# echo "Taking super user access"
 
-sudo -i
+# sudo -i
+
+echo "Deleting preinstalled node"
+sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm,node,man1/node}
 
 echo "Installing Brew and BrewCask"
-
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install caskroom/cask/brew-cask
 brew update && brew upgrade brew-cask
 
 echo "Installing Local Ruby"
-
 brew install ruby
 
-echo "Installing ZSH"
-
+echo "Installing Oh-my-ZSH"
 brew install zsh zsh-completions
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
 chsh -s /bin/zsh
 
 echo "Installing Softwares"
-
 brew install git
 brew cask install iterm2
 brew cask install google-chrome
@@ -29,19 +28,21 @@ brew cask install sublime-text
 brew cask install dropbox
 brew cask install vlc
 
-echo "Deleting preinstalled node"
-sudo rm -rf /usr/local/{lib/node{,/.npm,_modules},bin,share/man}/{npm,node,man1/node}
-
 echo "Installing Dependencies"
+
+# brew
 brew install node
 brew install ranger
 
+# npm
 npm install -g bower
 npm install -g gulpjs/gulp#4.0
 
+# gem
 gem install sass
 gem install compass
 
+# pip
 pip install Django==1.8.3
 pip install djangorestframework
 pip install fab
